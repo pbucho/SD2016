dad068072b48e093ee8d99285aeeb03e341dcd98
Miguel Amaral <miguel.amaral04@gmail.com>
2016-03-07 16:04:45 +0000

    better but still no works, lack of teachers help is indeed clear

--- lab3/ttt-rmi-server/src/main/java/ttt/Manager.java
+++ lab3/ttt-rmi-server/src/main/java/ttt/Manager.java
@@ -4,19 +4,22 @@ package ttt;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.net.MalformedURLException;
+import java.rmi.*;
 
 class Manager {
 	
 
-  public static void main(String args[]) {
-	  int registryPort = 58866;
-	  System.out.println("Port chosen: " + registryPort);
+  public static void main(String args[]) throws MalformedURLException {
+	  //int registryPort = 58866;
+	  //System.out.println("Port chosen: " + registryPort);
 	  try {
 		  TTT _game = new TTT();
           System.out.println("Created TTT object");
           
-          Registry reg = LocateRegistry.createRegistry(registryPort);
-          reg.rebind("TTT", _game);
+          //Registry reg = LocateRegistry.createRegistry(registryPort);
+          Naming.rebind("_game", _game);
+          //reg.rebind("_game", _game);
           System.out.println("TTT online");
 
           
@@ -24,7 +27,7 @@ class Manager {
 	  catch (RemoteException e){
 		  
 		  System.out.println(e.getMessage());
-		  System.out.println("Something nasty happened on the server, game is not there!");
+		  System.out.println("Something nasty happened while launching the server");
 	  }
   }
 }
